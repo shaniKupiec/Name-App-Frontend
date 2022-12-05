ac07b7873f234148a72b2b37bcb3bdb0ee9ee09b
shani kupiec <shani.kupiec@gmail.com>
2022-12-05 20:36:54 +0200

    putting store in comment

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./assets/styles/Global.scss";
-import { Provider } from "react-redux";
-import store from "./store4";
+// import { Provider } from "react-redux";
+// import store from "./store4";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <Provider store={store}>
+  // <Provider store={store}>
     <App />
-  </Provider>
+  // </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
